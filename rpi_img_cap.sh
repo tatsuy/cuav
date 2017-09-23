e7153a7531eae0d83e6ecefd07015ddb92af016f
Kawamura <kawamura@tajisoft.jp>
2017-09-23 10:33:58 +0000

    add ISO parameter

--- rpi_img_cap.sh
+++ rpi_img_cap.sh
@@ -4,4 +4,5 @@ QUALITY=100
 
 FILE_PREFIX=$(date "+%Y")
 ln -s latest_img.jpg fake_chameleon.pgm
-raspistill -r -l latest_img.jpg -q $QUALITY -v -dt -t 6000000 -tl 1000 -o camera/raw/${FILE_PREFIX}%dZ.jpg
+## TODO: check param, ISO:400
+raspistill -n -r -l latest_img.jpg -ISO 400 -q $QUALITY -v -dt -t 6000000 -tl 1000 -o /mnt/img/${FILE_PREFIX}%dZ.jpg
